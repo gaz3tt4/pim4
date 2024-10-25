d4abc2c9a2bf2e185c8842a9a842155be811edea
gaz3tt4 <110501617+gaz3tt4@users.noreply.github.com>
2024-10-25 08:06:22 -0300

    Update settings.py

--- PIM/settings.py
+++ PIM/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test_db',
         'USER': 'root',
-        'PASSWORD': 'carne7539@',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
