4ed7b8d08b05d513000cc573bca1773415efc42e
lucasGazetta <lucas@gazetta>
2024-10-26 16:03:09 -0300

    teste vw Show Clietes

--- clientes/views.py
+++ clientes/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from clientes.models import clientes
 
 # Create your views here.
 # função index retorna a viwe index html pasta template
 def index(request):
     template = loader.get_template('index.html')
     return HttpResponse(template.render())
+
+
+def ShowClientes(request):
+    client = clientes.objects.all().values()
+    templat = loader.get_template('showClientes.html')
+    context = {
+        'cliente': client
+    }
+    return HttpResponse(templat.render(context, request))
