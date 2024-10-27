7ca721a4faa7483a41907f858a2b2fd351806111
lucasGazetta <lucas@gazetta>
2024-10-27 11:19:27 -0300

    alteração show clientes/ manutenção das rotas

--- PIM/urls.py
+++ PIM/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from clientes import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index), # rota aponta para o methodo index
     path('', include('clientes.urls')) # rota aponta para o methodo index no arquivo views / clientes
 ]
