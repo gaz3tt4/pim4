3c6bddb7e11a3801911a8dcfb5e8f7b349abcaeb
GabrielAltobeli <glbeli@outlook.com>
2024-11-02 15:49:28 -0300

    ajustes em cadastroClientes e showClientes

--- clientes/models.py
+++ clientes/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Cliente(models.Model):
     cli_in_id = models.AutoField(primary_key=True)
-    cli_st_nome = models.CharField(max_length=50)
+    cli_st_nome = models.CharField(max_length=255)
     cli_ch_tipo = models.CharField(max_length=1)
     cli_st_doc = models.CharField(max_length=18)
     cli_st_endereco = models.CharField(max_length=150, null=True, blank=True)
--- clientes/urls.py
+++ clientes/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('index', views.index, name='index'),
     path('clientes/', views.ShowClientes, name='showClientes'),
     path('cadastroclientes', views.cadastro, name='cadastroClientes'),
     path('store', views.store, name='store'),# rota utilizada para gravar os dados no banco
