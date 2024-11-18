3c426c33d3de596e113e9bc6f48035ee0661243e
brennoBardella <brennobenebar@hotmail.com>
2024-11-17 21:13:04 -0300

    Produtos correção

--- produtos/urls.py
+++ produtos/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('produtos/', views.show_produtos, name='showProdutos'),
-    path('produtos/filtrar/', views.filtrar_produtos, name='filtroProdutos'),
-    path('produtos/cadastrar/', views.cadastrar_produto, name='cadastroProdutos'),
-    path('produtos/editar/<int:produto_id>/', views.editar_produto, name='editarProduto'),
-    path('produtos/excluir/<int:produto_id>/', views.excluir_produto, name='excluirProduto'),
+    path('produtos/', views.showProdutos, name='showProdutos'),
+    path('produtos/filtrar/', views.filtrarProdutos, name='filtroProdutos'),
+    path('produtos/cadastrar/', views.cadastrarProduto, name='cadastroProdutos'),
+    path('produtos/editar/<int:produto_id>/', views.editarProduto, name='editarProduto'),
+    path('produtos/excluir/<int:produto_id>/', views.excluirProduto, name='excluirProduto'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,20 +1,11 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Produto
-from .forms import ProdutoForm
+from .models import produtos
 
-def show_produtos(request):
+def showProdutos(request):
     produtos = Produto.objects.all()
-    return render(request, 'produtos.html', {'produtos': produtos})
+    return render(request, 'showProdutos.html', {'produtos': produtos})
 
-def filtrar_produtos(request):
-    # Lógica para filtrar produtos com base nos dados do formulário
-    if request.method == 'POST':
-        nome = request.POST.get('Nome')
-        produtos = Produto.objects.filter(nome__icontains=nome)
-        return render(request, 'produtos.html', {'produtos': produtos})
-    return redirect('showProdutos')
-
-def cadastrar_produto(request):
+def cadastrarProduto(request):
     if request.method == 'POST':
         form = ProdutoForm(request.POST)
         if form.is_valid():
@@ -22,9 +13,9 @@ def cadastrar_produto(request):
             return redirect('showProdutos')
     else:
         form = ProdutoForm()
-    return render(request, 'cadastro_produto.html', {'form': form})
+    return render(request, 'cadastroProduto.html', {'form': form})
 
-def editar_produto(request, produto_id):
+def editarProduto(request, produto_id):
     produto = get_object_or_404(Produto, id=produto_id)
     if request.method == 'POST':
         form = ProdutoForm(request.POST, instance=produto)
@@ -33,9 +24,9 @@ def editar_produto(request, produto_id):
             return redirect('showProdutos')
     else:
         form = ProdutoForm(instance=produto)
-    return render(request, 'editar_produto.html', {'form': form})
+    return render(request, 'editarProduto.html', {'form': form})
 
-def excluir_produto(request, produto_id):
+def excluirProduto(request, produto_id):
     produto = get_object_or_404(Produto, id=produto_id)
     produto.delete()
     return redirect('showProdutos')
