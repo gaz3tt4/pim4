c91df59df8e5342f730aebd9fbd45da3e9b82ed5
LucaAgostinho <141362364+RichMachine@users.noreply.github.com>
2024-11-22 23:05:08 -0300

    Validação Cadastro Cliente
    Criei o arquivo utils.py dentro do Clientes para melhor organização. Nele declarei funções que utilizo dentro da view de storeCliente.
    
    Ainda não foi feita a validação para view updateCliente.

--- PIM/settings.py
+++ PIM/settings.py
@@ -135,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
