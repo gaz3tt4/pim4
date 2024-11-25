75e510b8a0071636a274c11793551689ab377e7e
brennoBardella <brennobenebar@hotmail.com>
2024-11-24 23:23:33 -0300

    Autenticação login, ainda não finalizada

--- login/views.py
+++ login/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .utils import validar_login
 
 def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+
+        if validar_login(request, username, password):
+            return redirect('index')  # Redireciona para a página inicial
+        else:
+            return redirect('loginTemplate')  # Retorna para a página de login
 
     return render(request, 'login.html')
+
+def index(request):
+    return render(request, 'index.html')
