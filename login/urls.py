93da227c7f11d13d76bb4d887da541702e813f3d
lucasGazetta <lucas@gazetta>
2024-11-26 08:46:00 -0300

    mege index

--- login/urls.py
+++ login/urls.py
@@ -3,10 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.login, name='loginTemplate'),
-<<<<<<< HEAD
-    # path('index/', views.index, name='index'),
-=======
     path('index/', views.index, name='index'),
     path('login/', views.login, name='login'),
->>>>>>> 20cd9cccac38a9c3ed1eefac2013f029ec2698f6
 ]
