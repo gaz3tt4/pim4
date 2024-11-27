e0ea43d28e1b9adc397becac2e5696bbfa444385
LucaAgostinho <141362364+RichMachine@users.noreply.github.com>
2024-11-26 23:39:07 -0300

    Validação StoreFornecedor

---
+++ fornecedores/utils.py
@@ -0,0 +1,111 @@
+import re
+import requests
+from .models import Fornecedor
+from validate_docbr import CPF , CNPJ
+from django.contrib import messages
+
+def validar_nome(nome):
+
+    return not bool(re.search("[!@#$*()_+|?\/<>;.,1234567890\[\]{}=-]", nome))
+
+def validar_doc(tipo, doc, request):
+
+    if tipo == "F":
+
+        if len(doc) == 11:
+
+            cpf = CPF()
+
+            if cpf.validate(doc): return True
+
+            else:
+                messages.error(request, "CPF Inexistente.")
+                return False
+
+        else:
+            messages.error(request, "Tamanho do CPF é inválido.")
+            return False
+
+    elif tipo == "J":
+
+        if len(doc) == 14:
+
+            cnpj = CNPJ()
+
+            if cnpj.validate(doc): return True
+
+            else:
+                messages.error(request, "CNPJ Inexistente.")
+                return False
+
+        else:
+            messages.error(request, "Tamanho do CNPJ é inválido.")
+            return False
+
+    else:
+        messages.error(request, "Tipo inválido.")
+        return False
+
+def validar_endereco(endereco):
+
+    padrao = r"^(Rua|Avenida|Travessa|Alameda|Praça|Largo|Rodovia|Estrada|Beira-Mar|Passagem|Viaduto|Ponte|Caminho|Conjunto|Setor|Zona)\s+[A-Za-zà-úÀ-Ú\s]{1,55}\s+\d{1,5}$"
+
+    if re.match(padrao, endereco): return True
+
+    else: return False
+
+
+def validar_cidade(cidade_procurada, estado):
+
+    if re.match("^[A-Za-záéíóúãõâêîôûàèìòù\s]+$", cidade_procurada):
+
+        cidades = requests.get(f"https://servicodados.ibge.gov.br/api/v1/localidades/estados/{estado}/distritos")
+
+        cidades_json =cidades.json()
+
+        if cidades:
+
+            for linha in cidades_json:
+
+                if linha['nome'].lower() == cidade_procurada.lower():
+
+                    return True
+
+        return False
+
+    else: return False
+
+def validar_email(email):
+    return bool(re.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email))
+
+def sanitiza_telefone(telefone):
+    return re.sub(r'[^a-zA-Z0-9]', '', telefone)
+
+def validar_telefone(telefone):
+   if len(telefone) == 12 or len(telefone) == 13:
+    return True
+   else:
+    return False
+
+def edita_fornecedor(fornecedor, nome, doc, tipo, endereco, cidade, estado, telefone, email):
+    fornecedor.fnr_st_nome = nome
+    fornecedor.fnr_st_doc = doc
+    fornecedor.fnr_ch_tipo = tipo
+    fornecedor.fnr_st_endereco = endereco
+    fornecedor.fnr_st_cidade = cidade
+    fornecedor.fnr_st_estado = estado
+    fornecedor.fnr_st_telefone = telefone
+    fornecedor.fnr_st_email = email
+    fornecedor.save()
+
+def registra_fornecedor_no_banco(nome, doc, tipo, endereco, cidade, estado, telefone, email):
+    fornecedor = Fornecedor()
+    fornecedor.fnr_st_nome = nome
+    fornecedor.fnr_st_doc = doc
+    fornecedor.fnr_ch_tipo = tipo
+    fornecedor.fnr_st_endereco = endereco
+    fornecedor.fnr_st_cidade = cidade
+    fornecedor.fnr_st_estado = estado
+    fornecedor.fnr_st_telefone = telefone
+    fornecedor.fnr_st_email = email
+    fornecedor.save()
--- fornecedores/views.py
+++ fornecedores/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from fornecedores.models import Fornecedor
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
+from django.contrib import messages
+from .utils import validar_nome, validar_doc, validar_endereco, validar_cidade, sanitiza_telefone, validar_telefone, validar_email, registra_fornecedor_no_banco
 # Create your views here.
 
-# def index(request):
-#     template = loader.get_template('index.html')
-#     return HttpResponse(template.render())
 
 def ShowFornecedor(request):
     fornecedores = Fornecedor.objects.all()
@@ -22,17 +19,36 @@ def cadastro(request):
 
 def storeFornecedor(request):
     if request.method == 'POST':
-       fornecedor = Fornecedor()
-       fornecedor.fnr_st_nome = request.POST.get('Nome')
-       fornecedor.fnr_st_tipo = request.POST.get('Tipo')
-       fornecedor.fnr_st_doc = request.POST.get('Doc')   
-       fornecedor.fnr_st_endereco = request.POST.get('Endereco')
-       fornecedor.fnr_st_cidade = request.POST.get('Cidade')
-       fornecedor.fnr_st_estado = request.POST.get('Estado')
-       fornecedor.fnr_st_telefone = request.POST.get('Telefone')
-       fornecedor.fnr_st_email = request.POST.get('Email')
-       fornecedor.save()
-    return redirect('showFornecedor')
+        nome = request.POST.get('Nome')
+        if validar_nome(nome):
+            doc = request.POST.get('Doc')
+            tipo = request.POST.get('Tipo')
+            if validar_doc(tipo, doc, request):
+                endereco = request.POST.get('Endereco')
+                if validar_endereco(endereco):
+                    cidade = request.POST.get('Cidade')
+                    estado = request.POST.get('Estado')
+                    if validar_cidade(cidade, estado):
+                        email = request.POST.get('Email')
+                        if validar_email(email):
+                            telefone = sanitiza_telefone(request.POST.get('Telefone'))
+                            if validar_telefone(telefone):
+                                messages.success(request, 'Fornecedor cadastrado com sucesso.')
+                                registra_fornecedor_no_banco(nome, doc, tipo, endereco, cidade, estado, telefone, email)
+                                return redirect('showFornecedor')
+                            else:
+                                messages.error(request, "Telefone inválido")
+                        else:
+                            messages.error(request, "Email inválido.")
+                    else:
+                        messages.error(request, "Cidade inexistente.")
+                else:
+                    messages.error(request, "Endereço inválido.")
+            else:
+                pass
+        else:
+            messages.error(request, "Nome inválido.")
+    return render(request, 'cadastroFornecedor.html')
 
 def edit(request, pk):
     fornecedor = get_object_or_404(Fornecedor, fnr_in_id=pk)
@@ -46,7 +62,7 @@ def update(request, pk):
     fornecedor.Fornecedor = Fornecedor
     if request.method == 'POST':
         fornecedor.fnr_st_nome = request.POST.get('Nome')
-        fornecedor.fnr_st_doc = request.POST.get('Doc')   
+        fornecedor.fnr_st_doc = request.POST.get('Doc')
         fornecedor.fnr_st_endereco = request.POST.get('Endereco')
         fornecedor.fnr_st_cidade = request.POST.get('Cidade')
         fornecedor.fnr_st_estado = request.POST.get('Estado')
